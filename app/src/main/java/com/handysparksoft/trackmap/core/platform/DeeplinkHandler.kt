edfe38d8ca861fe1decb6067e56f38eee8ab3b61
davidasensio <davidasensio81@gmail.com>
2020-10-31 07:32:49 +0100

    Replace share intent with Android Sharesheet
    More info here:
     - https://developer.android.com/training/sharing/send#using-android-system-sharesheet

--- app/src/main/java/com/handysparksoft/trackmap/core/platform/DeeplinkHandler.kt
+++ app/src/main/java/com/handysparksoft/trackmap/core/platform/DeeplinkHandler.kt
@@ -1,6 +1,7 @@
 package com.handysparksoft.trackmap.core.platform
 
 import android.app.Activity
+import android.content.Intent
 import android.util.Base64
 import androidx.core.app.ShareCompat
 import java.nio.charset.Charset
@@ -15,10 +16,14 @@ object DeeplinkHandler {
     fun generateDeeplink(activity: Activity, code: String, name: String) {
         val encodedCode = encodeBase64(code)
         val joinTrackMapURL = "https://trackmap.firebaseapp.com?code=$encodedCode"
-        val shareIntent = ShareCompat.IntentBuilder.from(activity)
-            .setType("text/plain")
-            .setText("Join TrackMap \"$name\" here:\n$joinTrackMapURL")
-            .intent
+
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Join TrackMap \"$name\" here:\n$joinTrackMapURL")
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
         if (shareIntent.resolveActivity(activity.packageManager) != null) {
             activity.startActivity(shareIntent)
         }
