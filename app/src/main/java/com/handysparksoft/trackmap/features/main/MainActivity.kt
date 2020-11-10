f4cdca4480b1cf4d58faa03f5951d5d50f253662
davidasensio <davidasensio81@gmail.com>
2020-11-10 18:55:19 +0100

    Add toolbar to MainActivity (Only visible on Entries fragment)

--- app/src/main/java/com/handysparksoft/trackmap/features/main/MainActivity.kt
+++ app/src/main/java/com/handysparksoft/trackmap/features/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.handysparksoft.trackmap.features.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -10,6 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.handysparksoft.trackmap.R
 import com.handysparksoft.trackmap.core.extension.app
 import com.handysparksoft.trackmap.core.extension.startActivity
+import com.handysparksoft.trackmap.core.extension.toast
 import com.handysparksoft.trackmap.core.platform.Prefs
 import com.handysparksoft.trackmap.databinding.ActivityMainBinding
 import javax.inject.Inject
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupToolbar()
         setupUI()
     }
 
@@ -41,9 +45,32 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                toast("Profile icon clicked!")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_user_profile_white)
+    }
+
     private fun setupUI() {
         val navController = findNavController(R.id.fragment)
         binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.label) {
+                "Entries" -> supportActionBar?.show()
+                else -> supportActionBar?.hide()
+            }
+        }
     }
 
     companion object {
