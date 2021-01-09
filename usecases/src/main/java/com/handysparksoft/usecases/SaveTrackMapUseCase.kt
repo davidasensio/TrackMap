0062a8f718a5ee5b9f0a235b4dc4ecc36dc1995d
davidasensio <davidasensio81@gmail.com>
2021-01-09 01:41:45 +0100

    Check TrackMap generated code is available before use it

--- usecases/src/main/java/com/handysparksoft/usecases/SaveTrackMapUseCase.kt
+++ usecases/src/main/java/com/handysparksoft/usecases/SaveTrackMapUseCase.kt
@@ -1,9 +1,15 @@
 package com.handysparksoft.usecases
 
+import com.handysparksoft.data.Result
 import com.handysparksoft.data.repository.TrackMapRepository
 import com.handysparksoft.domain.model.TrackMap
 
 class SaveTrackMapUseCase(private val trackMapRepository: TrackMapRepository) {
     suspend fun execute(trackMapId: String, trackMap: TrackMap) =
         trackMapRepository.saveTrackMap(trackMapId, trackMap)
+
+    suspend fun checkIfTrackMapCodeAlreadyExists(trackMapId: String): Boolean {
+        val trackMap = trackMapRepository.getTrackMapById(trackMapId)
+        return (trackMap is Result.Success)
+    }
 }
