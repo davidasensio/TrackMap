d2e9877ee5fc0e0e63fa30f69129b659dbd3cba5
davidasensio <davidasensio81@gmail.com>
2021-01-11 18:19:46 +0100

    Fix crashes

--- domain/src/main/java/com/handysparksoft/domain/model/UserProfileData.kt
+++ domain/src/main/java/com/handysparksoft/domain/model/UserProfileData.kt
@@ -4,8 +4,8 @@ import java.io.Serializable
 
 data class UserProfileData(
     val userId: String,
-    val nickname: String,
-    val fullName: String,
-    val phone: String,
+    val nickname: String?,
+    val fullName: String?,
+    val phone: String?,
     val image: String? // Base64 format
 ) : Serializable
